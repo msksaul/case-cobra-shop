303b095b4c8059f32b961e24b9d95de233b80fe8
mscalel <msksaul@gmail.com>
2024-05-22 15:47:06 -0600

    Secure Payments

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -10,10 +10,21 @@ import { useMutation } from '@tanstack/react-query'
 import { ArrowRight, Check } from 'lucide-react'
 import { useState, useEffect } from 'react'
 import Confetti from 'react-dom-confetti'
+import { createCheckoutSession } from './actions'
+import { useRouter } from 'next/navigation'
+import { useToast } from '@/components/ui/use-toast'
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import LoginModal from '@/components/LoginModal'
 
-const DesignPreview = ({configuration}: {configuration: Configuration}) => {
+const DesignPreview = ({ configuration }: {configuration: Configuration}) => {
 
-  const [showConfetti, setShowConfetti] = useState(false)
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState<boolean>(false)
+
+  const { id } = configuration
+  const router = useRouter()
+  const { toast } = useToast()
+  const [showConfetti, setShowConfetti] = useState<boolean>(false)
+  const { user } = useKindeBrowserClient()
 
   const { color, model, finish, material } = configuration
 
@@ -25,9 +36,26 @@ const DesignPreview = ({configuration}: {configuration: Configuration}) => {
   if(material==='polycarbonate') totalPrice += PRODUCT_PRICES.material.polycarbonate
   if(finish === 'textured') totalPrice += PRODUCT_PRICES.finish.textured
 
-  const {} = useMutation({
+  const { mutate: createPaymentSession } = useMutation({
     mutationKey: ['get-checkout-session'],
+    mutationFn: createCheckoutSession,
+    onSuccess: (({url}) => {
+      if(url) router.push(url)
+      else throw new Error('Unable to retrieve payment URL')
+    }),
+    onError: () => {
+      toast({title: 'Something went wrong', description: 'There was an error on our end, Please try again.', variant: 'destructive'})
+    }
   })
+
+  const handleCheckout = () => {
+    if(user) {
+      createPaymentSession({ configId: id })
+    }
+    else {
+      localStorage.setItem('configurationId', id)
+    }
+  }
 
   useEffect(() => {
     setShowConfetti(true)
@@ -39,6 +67,8 @@ const DesignPreview = ({configuration}: {configuration: Configuration}) => {
       <div className='pointer-events-none select-none absolute inset-0 overflow-hidden flex justify-center' aria-hidden='true'>
         <Confetti active={showConfetti} config={{ elementCount: 200, spread: 90 }} />
       </div>
+
+      <LoginModal isOpen={isLoginModalOpen} setIsOpen={setIsLoginModalOpen}/>
 
       <div className='mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12'>
         <div className='sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2'>
@@ -113,7 +143,14 @@ const DesignPreview = ({configuration}: {configuration: Configuration}) => {
             </div>
 
             <div className='mt-8 flex justify-end pb-12'>
-              <Button className='px-4 sm:px-6 lg:px-8'>Check out <ArrowRight className='h-4 w-4 ml-1.5 inline'/></Button>
+              <Button 
+                className='px-4 sm:px-6 lg:px-8'
+                onClick={() => {
+                  handleCheckout()
+                }}
+              >
+                  Check out <ArrowRight className='h-4 w-4 ml-1.5 inline'/>
+              </Button>
             </div>
           </div>
         </div>
